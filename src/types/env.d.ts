e893f9d152b263c2816e0df15068b5686666f27c
TakayukiTooyama <t.tooyama107@gmail.com>
2022-02-22 20:54:57 +0900

    :fire: 今回使わないファイルを削除

--- src/types/env.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-declare namespace NodeJS {
-  // 使用しない場合は削除
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface ProcessEnv {}
-}
